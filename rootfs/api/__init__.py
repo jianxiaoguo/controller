ecc655bd6f49eccacdc6079c197fd83d4d356855
lijianguo <lijianguo1991@outlook.com>
2025-03-28 11:37:43 +0800

    chore(version): update api version

--- rootfs/api/__init__.py
+++ rootfs/api/__init__.py
@@ -3,5 +3,5 @@ The **api** Django app presents a RESTful web API for interacting with the **dry
 """
 from .settings.celery import app as celery_app
 
-__version__ = '2.3.0'
+__version__ = 'canary'
 __all__ = ('celery_app',)
